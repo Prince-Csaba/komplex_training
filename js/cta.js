3ed02f5e9af5d2b1df6932f73debf08b46ba2368
Prince Csaba <bolyaicsaba@gmail.com>
2023-03-27 19:11:08 +0200

    -

--- js/cta.js
+++ js/cta.js
@@ -1,12 +1,17 @@
 function pageLoad() {
   console.log("Cta loaded");
   let ctaButton = document.getElementById("eBook");
+  let eBookDiv = document.getElementById("eBookDiv");
+  let ctaTape = document.getElementsByClassName("ctaTape");
+  console.log(ctaTape); 
   console.log(ctaButton);
   const showForm = () => {
     console.log("Button clicked");    
     right.classList.add("d-none");
     form.classList.remove("d-none");
     form.classList.add("d-flex");
+    ctaTape[0].classList.add("d-none");
+    eBookDiv.classList.add("flexHCenter");
   }
   let right = document.getElementById("right");
   let form = document.getElementById("form");
